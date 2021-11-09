92cd1e2eb57cd35212de47f6540d307f23025249
Josh Sixsmith <josh.sixsmith@gmail.com>
2021-11-10 09:58:20 +1100

    Initial commit of bathy_datasets/__init__.py

---
+++ bathy_datasets/__init__.py
@@ -0,0 +1,10 @@
+try:
+    from importlib import metadata as _md
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as _md
+
+try:
+    __version__ = _md.version(__name__)
+except _md.PackageNotFoundError:
+    __version__ = "Not Installed"
