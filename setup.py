41691885288e3135081800d73127cdfadbcd4880
Josh Sixsmith <josh.sixsmith@gmail.com>
2021-11-10 08:06:35 +1100

    Initial commit of pyproject.toml setup.cfg setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name="bathy_datasets",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    url="https://github.com/ausseabed/bathy_datasets",
+    author="AusSeabed",
+    description="Prototype module for creating bathymetry datasets from the AusSeabed team",
+    keywords=[
+        "bathymetry",
+        "metadata",
+        "stac",
+        "dggs",
+    ],
+    # packages=find_packages(),
+    packages=["bath_datasets"],
+    install_requires=[
+        "numpy",
+        "attrs",
+        "pandas",
+        "numba",
+        "rhealpixdggs",
+        "h3",
+        "structlog",
+    ],
+    license="Apache",
+    zip_safe=False,
+)
