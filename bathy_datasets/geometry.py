404d332e5194d77f56da7c0ee31d43946a3a6eb4
Josh Sixsmith <josh.sixsmith@gmail.com>
2021-11-09 18:24:22 +1100

    Added implementations for retrieving rhealpix codes and geometry using the rhealpixdggs library.

--- bathy_datasets/geometry.py
+++ bathy_datasets/geometry.py
@@ -1,17 +1,34 @@
-import dask
+import dask.dataframe
 import pandas
 import h3
 from shapely.geometry import Polygon
+import geopandas
+from rhealpixdggs import dggs, ellipsoids
+from osgeo import osr
 import structlog
 
-from bathy_datasets import constants
+# from bathy_datasets import constants
 
 _LOG = structlog.get_logger()
 
 
+def _init_rhealpix():
+    """Initialise an rHEALPIX projection using the WGS84 parameters."""
+    crs = osr.SpatialReference()
+    crs.ImportFromEPSG(4326)
+
+    ellips = ellipsoids.Ellipsoid(a=crs.GetSemiMajor(), b=crs.GetSemiMinor())
+    rhealp = dggs.RHEALPixDGGS(ellips)
+
+    return rhealp
+
+
+RHEALPIX = _init_rhealpix()
+
+
 def lon_lat_2_h3(row: pandas.core.series.Series, resolution: int=15):
     """Convert a longitude,latitude pair to a H3 index code."""
-    code = h3.geo_to_h3(row.Y, row.X, resolution)
+    code = h3.geo_to_h3(row.latitude, row.longitude, resolution)
     return code
 
 
@@ -28,9 +45,9 @@ def h3_index_parallel(dataframe: pandas.DataFrame, npartitions: int=2) -> pandas
         return dataframe.apply((lambda row: lon_lat_2_h3(row)), axis=1)
 
     # dask doesn't like mutli-index dataframes
-    dask_data = dask.dataframe(dataframe.reset_index(), npartitions=npartitions)
+    dask_data = dask.dataframe.from_pandas(dataframe.reset_index(), npartitions=npartitions)
 
-    return dask_data.mpa_partitions(_wrap).compute()
+    return dask_data.map_partitions(_wrap).compute()
 
 
 def h3_cell_count(dataframe: pandas.DataFrame) -> pandas.DataFrame:
@@ -45,9 +62,9 @@ def h3_cell_geometry_parallel(dataframe: pandas.DataFrame, npartitions: int=2):
     def _wrap(dataframe):
         return dataframe.apply((lambda row: h3_cell_geometry(row)), axis=1)
 
-    dask_data = dask.dataframe(dataframe, npartitions=npartitions)
+    dask_data = dask.dataframe.from_pandas(dataframe, npartitions=npartitions)
 
-    return dask_data.mpa_partitions(_wrap).compute()
+    return dask_data.map_partitions(_wrap).compute()
 
 
 def dissolve(geodataframe: geopandas.GeoDataFrame) -> geopandas.GeoDataFrame:
@@ -55,7 +72,27 @@ def dissolve(geodataframe: geopandas.GeoDataFrame) -> geopandas.GeoDataFrame:
     geodataframe["dissolve_field"] = 1
     dissolved = geodataframe.dissolve(by='dissolve_field')
 
-    dissolved.reset_index(drop=True, inplace=True
+    dissolved.reset_index(drop=True, inplace=True)
     geodataframe.drop("dissolve_field", axis=1, inplace=True)
 
     return dissolved["geometry"]
+
+
+def rhealpix_id_geom(row: pandas.core.series.Series, resolution: int=15):
+    """Convert a longitude,latitude pair to a rHEALPIX index code and geometry."""
+    cell = RHEALPIX.cell_from_point(resolution, (row.longitude, row.latitude), False)
+
+    idx = "".join(map(str, cell.suid))
+    polygon = Polygon(cell.vertices(plane=False))
+
+    return idx, polygon
+
+
+def rhealpix_parallel(dataframe: pandas.DataFrame, npartitions: int=2):
+    """Return the rHEALPIX indexes and geometry."""
+    def _wrap(dataframe):
+        return dataframe.apply((lambda row: rhealpix_id_geom(row)), axis=1)
+
+    dask_data = dask.dataframe.from_pandas(dataframe.reset_index(), npartitions=npartitions)
+
+    return dask_data.map_partitions(_wrap).compute()
